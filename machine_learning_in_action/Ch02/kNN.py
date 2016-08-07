69b22bf2ac225baa0f940fd6e3119a9e6b187c69
luy <yi_lu@foxitsoftware.com>
2016-08-07 05:17:48 -0700

    Add kNN to git.

---
+++ machine_learning_in_action/Ch02/kNN.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import numpy
+import operator
+
+def createDataSet():
+  group = ([1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1])
+  labels = ['A', 'A', 'B', 'B']
+  return group, labels
+
+
+
